33aeb4d3d95c01e199c81539fb0b07e829502d51
Andres Rodriguez <anrocox@gmail.com>
2014-07-16 10:47:37 -0500

    How to get the size of a string in python?

---
+++ src/length_str.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 15, 2014
+
+@author: anroco
+
+How to get the size of a string in python?
+
+¿Como obtener el tamaño de un string en python?
+'''
+
+#create a str
+s = 'I thought a thought.'
+print(s)
+
+#know the length of a str, uses the len() function built-in python
+print (len(s))
+
+#create a str
+s = 'I saw a saw that could out saw any other saw I ever saw.'
+print(s)
+
+#know the length of a str
+print (len(s))
